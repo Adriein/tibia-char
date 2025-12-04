1de2e6ea2937e88ab284d316324caff8d388bba3
Adria Claret <adria.claret@gmail.com>
2025-12-04 18:27:31 +0100

    feat: auction link set helpers

--- api/internal/scrap/scrap.go
+++ api/internal/scrap/scrap.go
@@ -2,33 +2,22 @@ package scrap
 
 type BazaarAuctionLinkSet map[int]string
 
-func (v BazaarAuctionLinkSet) Get(key string) string {
-	vs := v[key]
-	if len(vs) == 0 {
-		return ""
-	}
-	return vs[0]
+func (set BazaarAuctionLinkSet) Get(key int) (string, bool) {
+	value, ok := set[key]
+
+	return value, ok
 }
 
-// Set sets the key to value. It replaces any existing
-// values.
-func (v BazaarAuctionLinkSet) Set(key, value string) {
-	v[key] = []string{value}
+func (set BazaarAuctionLinkSet) Set(key int, value string) {
+	set[key] = value
 }
 
-// Add adds the value to key. It appends to any existing
-// values associated with key.
-func (v BazaarAuctionLinkSet) Add(key, value string) {
-	v[key] = append(v[key], value)
+func (set BazaarAuctionLinkSet) Del(key int) {
+	delete(set, key)
 }
 
-// Del deletes the values associated with key.
-func (v BazaarAuctionLinkSet) Del(key string) {
-	delete(v, key)
-}
+func (set BazaarAuctionLinkSet) Has(key int) bool {
+	_, ok := set[key]
 
-// Has checks whether a given key is set.
-func (v BazaarAuctionLinkSet) Has(key string) bool {
-	_, ok := v[key]
 	return ok
 }
