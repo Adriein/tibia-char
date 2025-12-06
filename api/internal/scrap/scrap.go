c2f14c389d530d2e621c0fd1ec85d38a2f47898b
Adria Claret <adria.claret@gmail.com>
2025-12-06 10:48:34 +0100

    feat: created obj to store char detail

--- api/internal/scrap/scrap.go
+++ api/internal/scrap/scrap.go
@@ -1,5 +1,7 @@
 package scrap
 
+import "time"
+
 type BazaarAuctionLinkSet map[int]string
 
 func (set BazaarAuctionLinkSet) Get(key int) (string, bool) {
@@ -20,4 +22,116 @@ func (set BazaarAuctionLinkSet) Has(key int) bool {
 	_, ok := set[key]
 
 	return ok
+}
+
+type ImgDisplay struct {
+	Link string
+	Name string
+}
+
+type BazaarCharAuctionDetail struct {
+	AuctionDetails struct {
+		Name       string
+		Level      int
+		Vocation   string
+		Gender     string
+		World      string
+		Highlights []string
+	}
+	General struct {
+		Mounts               int
+		Outfits              int
+		CreationDate         time.Time
+		Gold                 int
+		RegularWorldTransfer string
+		Skills               struct {
+			AxeFighting      int
+			ClubFighting     int
+			DistanceFighting int
+			Fishing          int
+			FistFighting     int
+			MagicLevel       int
+			Shielding        int
+			SwordFighting    int
+		}
+		Charms struct {
+			CharmExpansion            string
+			AvailableCharmPoints      int
+			SpentCharmPoints          int
+			AvailableMinorCharmEchoes int
+			SpentMinorCharmEchoes     int
+		}
+		HuntingTasks struct {
+			TaskPoints                   int
+			PermanentWeeklyTaskExpansion string
+			PermanentPreySlots           int
+			PreyWildcards                int
+		}
+		Hirelings struct {
+			Amount  int
+			Jobs    int
+			Outfits int
+		}
+		ExaltedDust             string
+		AnimusMasteriesUnlocked int
+		BossPoints              int
+		BonusPromotionPoints    int
+	}
+	ItemSummary []struct {
+		Img    string
+		Amount int
+		Name   string
+	}
+	StoreItemSummary []struct {
+		Img    string
+		Amount int
+		Name   string
+	}
+	Mounts       []ImgDisplay
+	StoreMounts  []ImgDisplay
+	Outfits      []ImgDisplay
+	StoreOutfits []ImgDisplay
+	Imbuements   []string
+	Charms       []struct {
+		Cost  int
+		Type  string
+		Name  string
+		Grade int
+	}
+	Quests   []string
+	Bestiary []struct {
+		Step    int
+		Kills   int
+		Name    string
+		Mastery bool
+	}
+	Bosstiary []struct {
+		Step  int
+		Kills int
+		Name  string
+	}
+	BountyTalisman struct {
+		Points int
+		Bounty []struct {
+			Name  string
+			Level int
+			Value float64
+		}
+	}
+	RevealedGems []struct {
+		Gem  string
+		Mod1 ImgDisplay
+		Mod2 ImgDisplay
+		Mod3 ImgDisplay
+	}
+	FragmentProgress []struct {
+		Grade      string
+		SupremeMod string
+	}
+	Proficiencies []struct {
+		Weapon        string
+		Level         string
+		TotalProgress int
+		Mastery       bool
+	}
 }
