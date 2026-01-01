240f793dc6fa71e81ebabe7d6f76ff674fc8b7c8
Adria Claret <adria.claret@gmail.com>
2026-01-01 09:52:20 +0100

    feat: updated rollback migration with new table

--- app/database/migrations/000001_CreateAuctionTable.down.sql
+++ app/database/migrations/000001_CreateAuctionTable.down.sql
@@ -4,7 +4,9 @@ ALTER TABLE tc_auction DROP CONSTRAINT fk_world_auction;
 
 ALTER TABLE tc_auction_recording DROP CONSTRAINT fk_recording_auction;
 ALTER TABLE tc_skills DROP CONSTRAINT fk_skills_auction_recording;
+ALTER TABLE tc_charm DROP CONSTRAINT fk_charm_auction_recording;
 
+DROP TABLE tc_charm;
 DROP TABLE tc_skills;
 DROP TABLE tc_featured_items;
 DROP TABLE tc_auction_recording;
