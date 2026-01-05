2e661dbe693734d59c159cd4a894dd6630caaf96
Adria Claret <adria.claret@gmail.com>
2026-01-05 19:45:23 +0100

    feat: wip currency api integration

---
+++ app/pkg/vendor/currency-api.go
@@ -0,0 +1,66 @@
+package vendor
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"net/http"
+
+	"github.com/rotisserie/eris"
+)
+
+type CurrencyConversionRates struct {
+	Result     string `json:"result"`
+	ErrorType  string `json:"error-type"`
+	LastUpdate string `json:"time_last_update_utc"`
+	Rates      struct {
+		USD float64 `json:"USD"`
+		EUR float64 `json:"EUR"`
+		AUD float64 `json:"AUD"`
+		GBP float64 `json:"GBP"`
+		PLN float64 `json:"PLN"`
+		BRL float64 `json:"BRL"`
+	} `json:"conversion_rates"`
+}
+
+type OpenCurrencyAPI struct{}
+
+func NewOpenCurrencyAPI() *OpenCurrencyAPI {
+	return &OpenCurrencyAPI{}
+}
+
+func (o *OpenCurrencyAPI) GetConversionRates(ctx context.Context, currency string) (*CurrencyConversionRates, error) {
+	url := fmt.Sprintf("https://open.er-api.com/v6/latest/%s", currency)
+
+	request, err := http.NewRequestWithContext(ctx, http.MethodGet, url, nil)
+
+	if err != nil {
+		return nil, eris.Wrapf(err, "Error creating request for currency: %s", currency)
+	}
+
+	httpRes, err := http.DefaultClient.Do(request)
+
+	if err != nil {
+		return nil, eris.Wrapf(err, "Failed to fetch currency: %s", currency)
+	}
+
+	defer httpRes.Body.Close()
+
+	if httpRes.StatusCode != http.StatusOK {
+		return nil, eris.Errorf("open.er-api.com responded with http code %d fetching currency %s", httpRes.StatusCode, currency)
+	}
+
+	var response CurrencyConversionRates
+
+	err = json.NewDecoder(httpRes.Body).Decode(&response)
+
+	if err != nil {
+		return nil, eris.Wrapf(err, "Error fetching currency: %s", currency)
+	}
+
+	if response.Result != "success" {
+		return nil, eris.Errorf("Error fetching currency %s with status %s and type %s", currency, response.Result, response.ErrorType)
+	}
+
+	return &response, nil
+}
