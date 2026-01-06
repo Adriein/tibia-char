308554e5c57ee911fa83ff7dc10550a78b761885
Adria Claret <adria.claret@gmail.com>
2026-01-06 11:37:12 +0100

    feat: upd repository to create a get latest query

--- app/internal/currency/repository.go
+++ app/internal/currency/repository.go
@@ -4,10 +4,13 @@ import (
 	"context"
 	"database/sql"
 	"time"
+
+	"github.com/rotisserie/eris"
 )
 
 type CurrencyRepository interface {
 	Save(ctx context.Context, currency *CurrencyRate) error
+	GetLatest(ctx context.Context) (*CurrencyRate, error)
 }
 
 type PgCurrencyRepository struct {
@@ -44,5 +47,50 @@ func (r *PgCurrencyRepository) Save(ctx context.Context, currency *CurrencyRate)
 		currency.DateUpd.Format(time.DateTime),
 	)
 
-	return err
+	if err != nil {
+		return eris.Wrap(err, "Error saving currency rates")
+	}
+
+	return nil
+}
+
+func (r *PgCurrencyRepository) GetLatest(ctx context.Context) (*CurrencyRate, error) {
+	query := `
+		SELECT
+			tcr_usd,
+      tcr_eur,
+      tcr_aud,
+      tcr_gbp,
+      tcr_pln,
+      tcr_brl,
+      tcr_date_upd
+		FROM
+			tc_currency_rates
+		ORDER BY
+			tcr_date_upd DESC
+		LIMIT 1
+		;
+	`
+
+	row := r.connection.QueryRowContext(ctx, query)
+
+	var currency CurrencyRate
+
+	err := row.Scan(
+		&currency.USD,
+		&currency.EUR,
+		&currency.AUD,
+		&currency.GBP,
+		&currency.PLN,
+		&currency.BRL,
+		&currency.DateUpd,
+	)
+
+	currency.DateUpd = currency.DateUpd.In(time.UTC)
+
+	if err != nil {
+		return nil, eris.Wrap(err, "Failed to query currency rates")
+	}
+
+	return &currency, nil
 }
