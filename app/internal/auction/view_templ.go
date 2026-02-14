22ee833f8bfbc2b1d64f44ef75bf8ed60f2b55fe
Adria Claret <adria.claret@gmail.com>
2026-02-14 11:28:49 +0100

    feat: tweaks on card grid

--- app/internal/auction/view_templ.go
+++ app/internal/auction/view_templ.go
@@ -724,7 +724,7 @@ func Auctions(page *PaginatedAuctions) templ.Component {
 		if templ_7745c5c3_Err != nil {
 			return templ_7745c5c3_Err
 		}
-		templ_7745c5c3_Err = templruntime.WriteString(templ_7745c5c3_Buffer, 50, " / page</span></div></div><div class=\"grid-cards\">")
+		templ_7745c5c3_Err = templruntime.WriteString(templ_7745c5c3_Buffer, 50, " / page</span></div></div><div class=\"grid-auction-cards\">")
 		if templ_7745c5c3_Err != nil {
 			return templ_7745c5c3_Err
 		}
