08d23ef83ab71b8271d49b71d522347e21e8d063
Adria Claret <adria.claret@gmail.com>
2026-04-12 10:37:19 +0200

    feat: upd stats aggregation entry point

--- app/cmd/cron/stats/main.go
+++ app/cmd/cron/stats/main.go
@@ -3,56 +3,17 @@ package main
 import (
 	"context"
 	"log"
-	"log/slog"
-	"os"
-	"time"
 
 	"github.com/adriein/tibia-char/internal"
-	"github.com/adriein/tibia-char/internal/auction"
-	"github.com/adriein/tibia-char/internal/currency"
-	"github.com/adriein/tibia-char/pkg/constants"
 	"github.com/adriein/tibia-char/pkg/helper"
 	"github.com/adriein/tibia-char/pkg/middleware"
-	"github.com/adriein/tibia-char/pkg/vendor"
 	"github.com/rotisserie/eris"
 )
 
 func main() {
 	app := internal.NewApp()
 
-	opts := &slog.HandlerOptions{
-		ReplaceAttr: func(groups []string, attr slog.Attr) slog.Attr {
-			if attr.Key == slog.TimeKey {
-				formatted := attr.Value.Time().UTC().Format(time.DateTime)
-
-				return slog.String(slog.TimeKey, formatted)
-			}
-
-			return attr
-		},
-	}
-
-	var logger *slog.Logger
-
-	if os.Getenv(constants.Env) == constants.DEV {
-		logger = slog.New(slog.NewTextHandler(os.Stdout, opts))
-	} else {
-		logger = slog.New(slog.NewJSONHandler(os.Stdout, opts))
-	}
-
-	auctionRepository := auction.NewPgAuctionRepository(app.Database)
-	worldRepository := auction.NewPgWorldRepository(app.Database)
-	currencyRepository := currency.NewPgCurrencyRepository(app.Database)
-	aggAuctionRepository := auction.NewPgAggAuctionRepository(app.Database)
-
-	scrapperFactory := &auction.CollyFactory{}
-	parserFactory := &auction.HtmlParserFactory{}
-
-	tibiaAPI := vendor.NewTibiaApi()
-
-	mapper := auction.NewMapper(worldRepository)
-
-	service := auction.NewService(tibiaAPI, auctionRepository, worldRepository, currencyRepository, aggAuctionRepository, mapper, parserFactory, scrapperFactory, logger)
+	service := app.Modules.Auction
 
 	ctx := context.WithValue(context.Background(), middleware.TraceIDKey, helper.TraceID())
 
