2d68ad13137e6810afec0b3939d076d511de985a
Adria Claret <adria.claret@gmail.com>
2026-04-12 10:34:36 +0200

    feat: introduced module concept

--- app/cmd/cron/scrapper/main.go
+++ app/cmd/cron/scrapper/main.go
@@ -3,17 +3,11 @@ package main
 import (
 	"context"
 	"log"
-	"log/slog"
-	"os"
 	"time"
 
 	"github.com/adriein/tibia-char/internal"
-	"github.com/adriein/tibia-char/internal/auction"
-	"github.com/adriein/tibia-char/internal/currency"
-	"github.com/adriein/tibia-char/pkg/constants"
 	"github.com/adriein/tibia-char/pkg/helper"
 	"github.com/adriein/tibia-char/pkg/middleware"
-	"github.com/adriein/tibia-char/pkg/vendor"
 	_ "github.com/lib/pq"
 	"github.com/rotisserie/eris"
 )
@@ -21,48 +15,14 @@ import (
 func main() {
 	app := internal.NewApp()
 
-	auctionRepository := auction.NewPgAuctionRepository(app.Database)
-	worldRepository := auction.NewPgWorldRepository(app.Database)
-	currencyRepository := currency.NewPgCurrencyRepository(app.Database)
-	aggAuctionRepository := auction.NewPgAggAuctionRepository(app.Database)
-
-	scrapperFactory := &auction.CollyFactory{}
-	parserFactory := &auction.HtmlParserFactory{}
-
-	tibiaAPI := vendor.NewTibiaApi()
-
-	mapper := auction.NewMapper(worldRepository)
-
 	for true {
 		traceID := helper.TraceID()
 
 		ctx := context.WithValue(context.Background(), middleware.TraceIDKey, traceID)
 
-		opts := &slog.HandlerOptions{
-			ReplaceAttr: func(groups []string, attr slog.Attr) slog.Attr {
-				if attr.Key == slog.TimeKey {
-					formatted := attr.Value.Time().UTC().Format(time.DateTime)
+		service := app.Modules.Auction
 
-					return slog.String(slog.TimeKey, formatted)
-				}
-
-				return attr
-			},
-		}
-
-		var logger *slog.Logger
-
-		if os.Getenv(constants.Env) == constants.DEV {
-			logger = slog.New(slog.NewTextHandler(os.Stdout, opts)).With("source", "SCRAPPER", "trace_id", traceID)
-		} else {
-			logger = slog.New(slog.NewJSONHandler(os.Stdout, opts)).With("source", "SCRAPPER", "trace_id", traceID)
-		}
-
-		service := auction.NewService(tibiaAPI, auctionRepository, worldRepository, currencyRepository, aggAuctionRepository, mapper, parserFactory, scrapperFactory, logger)
-
-		err := service.ScrapperOrchestrator(ctx)
-
-		if err != nil {
+		if err := service.ScrapperOrchestrator(ctx); err != nil {
 			log.Println(eris.ToString(err, true))
 		}
 
--- app/internal/app.go
+++ app/internal/app.go
@@ -3,16 +3,27 @@ package internal
 import (
 	"database/sql"
 	"log"
+	"log/slog"
 	"os"
+	"time"
 
 	"github.com/adriein/tibia-char/database"
+	"github.com/adriein/tibia-char/internal/auction"
+	"github.com/adriein/tibia-char/internal/currency"
 	"github.com/adriein/tibia-char/pkg/constants"
 	"github.com/adriein/tibia-char/pkg/helper"
+	"github.com/adriein/tibia-char/pkg/vendor"
 	"github.com/joho/godotenv"
 )
 
+type Modules struct {
+	Auction auction.AuctionService
+}
+
 type App struct {
 	Database *sql.DB
+	Modules  *Modules
+	Logger   *slog.Logger
 }
 
 func NewApp() *App {
@@ -36,7 +47,54 @@ func NewApp() *App {
 		log.Fatal(envCheckerErr.Error())
 	}
 
+	logger := initLogger()
+
+	db := database.New()
+	modules := initModules(db, logger)
+
 	return &App{
-		Database: database.New(),
+		Database: db,
+		Modules:  modules,
+		Logger:   logger,
+	}
+}
+
+func initLogger() *slog.Logger {
+	opts := &slog.HandlerOptions{
+		ReplaceAttr: func(groups []string, attr slog.Attr) slog.Attr {
+			if attr.Key == slog.TimeKey {
+				formatted := attr.Value.Time().UTC().Format(time.DateTime)
+
+				return slog.String(slog.TimeKey, formatted)
+			}
+
+			return attr
+		},
+	}
+
+	if os.Getenv(constants.Env) == constants.DEV {
+		return slog.New(slog.NewTextHandler(os.Stdout, opts))
+	}
+
+	return slog.New(slog.NewJSONHandler(os.Stdout, opts))
+
+}
+
+func initModules(db *sql.DB, logger *slog.Logger) *Modules {
+	auctionRepository := auction.NewPgAuctionRepository(db)
+	worldRepository := auction.NewPgWorldRepository(db)
+	currencyRepository := currency.NewPgCurrencyRepository(db)
+	aggAuctionRepository := auction.NewPgAggAuctionRepository(db)
+
+	scrapperFactory := &auction.CollyFactory{}
+	parserFactory := &auction.HtmlParserFactory{}
+
+	tibiaAPI := vendor.NewTibiaApi()
+
+	mapper := auction.NewMapper(worldRepository)
+	service := auction.NewService(tibiaAPI, auctionRepository, worldRepository, currencyRepository, aggAuctionRepository, mapper, parserFactory, scrapperFactory, logger)
+
+	return &Modules{
+		Auction: service,
 	}
 }
