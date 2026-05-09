0e87f7857af4e76e6f961007b627bd3db2baf48c
Adria Claret <adria.claret@gmail.com>
2026-05-09 17:45:12 +0200

    feat: removed 2 working todos

--- app/internal/web/auction_controller.go
+++ app/internal/web/auction_controller.go
@@ -31,8 +31,6 @@ func (c *AuctionController) Get() gin.HandlerFunc {
 
 		var rawFilter auction.UrlQueryParamsDto
 
-		//TODO: make sure pagination takes into account the filters
-		//TODO: when hot deal + best deal appear when 2 filters and hot deal filter but not on good deal filter
 		//TODO: make all filters work
 		if err := ctx.ShouldBindQuery(&rawFilter); err != nil {
 			//TODO: Temporal logging until I decide what to do
