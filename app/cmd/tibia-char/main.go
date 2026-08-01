85bf2d987e508a36e57fbddf5f5067b22f0542e0
Adria Claret <adria.claret@gmail.com>
2026-08-01 10:28:11 +0200

    feat: created a custom db driver to log slow queries

--- app/cmd/tibia-char/main.go
+++ app/cmd/tibia-char/main.go
@@ -10,7 +10,6 @@ import (
 	"github.com/adriein/tibia-char/internal"
 	"github.com/adriein/tibia-char/internal/server"
 	"github.com/adriein/tibia-char/pkg/constants"
-	_ "github.com/lib/pq"
 )
 
 func main() {
--- app/database/database.go
+++ app/database/database.go
@@ -3,25 +3,124 @@ package database
 import (
 	"context"
 	"database/sql"
+	"database/sql/driver"
 	"log/slog"
 	"os"
 	"time"
 
 	"github.com/adriein/tibia-char/pkg/constants"
+	"github.com/lib/pq"
 	"github.com/rotisserie/eris"
 )
+
+type slowQueryConnector struct {
+	driver *slowQueryDriver
+	dsn    string
+}
+
+func (c *slowQueryConnector) Connect(ctx context.Context) (driver.Conn, error) {
+	return c.driver.Open(c.dsn)
+}
+
+func (c *slowQueryConnector) Driver() driver.Driver {
+	return c.driver
+}
+
+type slowQueryDriver struct {
+	driver.Driver
+	logger    *slog.Logger
+	threshold time.Duration
+}
+
+func (d *slowQueryDriver) Open(name string) (driver.Conn, error) {
+	conn, err := d.Driver.Open(name)
+
+	if err != nil {
+		return nil, err
+	}
+
+	return &customConnection{Conn: conn, logger: d.logger, threshold: d.threshold}, nil
+}
+
+type customConnection struct {
+	driver.Conn
+	logger    *slog.Logger
+	threshold time.Duration
+}
+
+func (c *customConnection) QueryContext(ctx context.Context, query string, args []driver.NamedValue) (driver.Rows, error) {
+	start := time.Now()
+
+	var rows driver.Rows
+	var err error
+
+	if queryer, ok := c.Conn.(driver.QueryerContext); ok {
+		rows, err = queryer.QueryContext(ctx, query, args)
+	} else {
+		dargs := make([]driver.Value, len(args))
+		for i, nv := range args {
+			dargs[i] = nv.Value
+		}
+		rows, err = c.Conn.(driver.Queryer).Query(query, dargs)
+	}
+
+	duration := time.Since(start)
+
+	if duration >= c.threshold {
+		c.logger.Warn("Slow query detected",
+			slog.Duration("duration", duration),
+			slog.String("query", query),
+			slog.Any("error", err),
+		)
+	}
+
+	return rows, err
+}
+
+func (c *customConnection) ExecContext(ctx context.Context, query string, args []driver.NamedValue) (driver.Result, error) {
+	start := time.Now()
+
+	var res driver.Result
+	var err error
+
+	if execer, ok := c.Conn.(driver.ExecerContext); ok {
+		res, err = execer.ExecContext(ctx, query, args)
+	} else {
+		dargs := make([]driver.Value, len(args))
+		for i, nv := range args {
+			dargs[i] = nv.Value
+		}
+		res, err = c.Conn.(driver.Execer).Exec(query, dargs)
+	}
+
+	duration := time.Since(start)
+
+	if duration >= c.threshold {
+		c.logger.Warn("Slow query detected",
+			slog.Duration("duration", duration),
+			slog.String("query", query),
+			slog.Any("error", err),
+		)
+	}
+
+	return res, err
+}
 
 func New(logger *slog.Logger) *sql.DB {
 	databaseDsn := os.Getenv(constants.DatabaseUrl)
 
-	database, err := sql.Open("postgres", databaseDsn)
-
-	if err != nil {
-		enhancedErr := eris.Wrap(err, "Failed db open conn on db init")
-
-		logger.Error(eris.ToString(enhancedErr, true))
-		os.Exit(1)
+	wrappedDriver := &slowQueryDriver{
+		Driver:    &pq.Driver{},
+		logger:    logger,
+		threshold: 10 * time.Second,
 	}
+
+	connector := &slowQueryConnector{
+		driver: wrappedDriver,
+		dsn:    databaseDsn,
+	}
+
+	database := sql.OpenDB(connector)
 
 	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
 
